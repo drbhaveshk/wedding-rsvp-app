25112397c2fda5005de8496a4a4e9e0c543ecabc
drbhaveshk <drbhaveshk@gmail.com>
2025-12-09 11:22:38 +0530

    Add sendWhatsAppTemplateInvitation to imports

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const { 
-  sendWhatsAppInvitation, 
+  sendWhatsAppInvitation,
+  sendWhatsAppTemplateInvitation,
   sendRSVPConfirmation,
   sendMediaInvitation,
   sendWhatsAppTextMessage,
