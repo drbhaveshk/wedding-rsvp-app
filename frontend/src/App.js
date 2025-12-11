c0853c3b0536e3a42649f2e35b9dc690ce64d112
drbhaveshk <drbhaveshk@gmail.com>
2025-12-11 11:29:02 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RSVPForm from './components/RSVPForm';
-import AdminPanel from './components/AdminPanel';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RSVPForm1 from './components/RSVPForm1';
+import RSVPForm2 from './components/RSVPForm2';
+import RSVPForm3 from './components/RSVPForm3';
+import MultiWeddingAdminPanel from './components/MultiWeddingAdminPanel';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RSVPForm />} />
-        <Route path="/rsvp" element={<RSVPForm />} />
-        <Route path="/admin" element={<AdminPanel />} />
+        {/* RSVP Forms */}
+        <Route path="/rsvp/wedding1" element={<RSVPForm1 />} />
+        <Route path="/rsvp/wedding2" element={<RSVPForm2 />} />
+        <Route path="/rsvp/wedding3" element={<RSVPForm3 />} />
+        
+        {/* Admin Panel */}
+        <Route path="/admin" element={<MultiWeddingAdminPanel />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
-
-export default App;
